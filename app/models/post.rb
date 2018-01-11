a4f143c46460e888daa8960d41729ca478326a64
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-11 21:49:54 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,17 @@ class Post < ActiveRecord::Base
   validates :content, length: { is: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :title_must_be_clickbait
+
+  def title_must_be_clickbait
+    if title
+      if !title.include?("Won't Believe") &&
+        !title.include?("Secret") &&
+        !title.include?("Top 3") &&
+        !title.include?("Guess")
+        errors.add(:title, "must contain clickbait!")
+  end
+end
+end
+
 end
